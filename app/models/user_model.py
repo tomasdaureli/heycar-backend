ea96bff26f0f8eb5ac3529180685bf2f54ab6d78
tomasdaureli <tomasdaureli@gmail.com>
2024-11-06 23:01:30 -0300

    add endpoint to save notification token

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -15,7 +15,6 @@ class User(Base):
     points = Column(Integer, default=0, nullable=True)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    badges = relationship('Badge', back_populates='user')
-
-
-
+    badges = relationship("Badge", back_populates="user")
+    notification_token = Column(String(255), nullable=True)
+    notification_type = Column(String(255), nullable=True)
---
+++ app/sql-schemas/v8-notification-token.sql
@@ -0,0 +1,3 @@
+ALTER TABLE users ADD COLUMN notification_token VARCHAR(255) NULL;
+
+ALTER TABLE users ADD COLUMN notification_type VARCHAR(255) NULL;
