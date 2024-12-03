b3dee1535d1943623d515287537e09f42e1b1497
franco <francorep2@gmail.com>
2024-12-03 00:50:26 -0300

    add translate

--- odb_simulator/odb_sim.py
+++ odb_simulator/odb_sim.py
@@ -1,16 +1,21 @@
 import os
 import random
 import json
-import requests
 import time
+from deep_translator import GoogleTranslator
+
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 API_URL = os.getenv("API_URL")
 VEHICLE_ID = os.getenv("VEHICLE_ID")
-INTERVAL_SECONDS = os.getenv("INTERVAL_SECONDS")
+INTERVAL_SECONDS = int(os.getenv("INTERVAL_SECONDS", 5))
 
-with open('app\obd-trouble-codes.json') as f:
+if not API_URL or not VEHICLE_ID:
+    raise ValueError("API_URL y VEHICLE_ID deben estar configurados en el archivo .env")
+
+with open('app/obd-trouble-codes.json') as f:
     odb_codes = json.load(f)
 
 status_options = ["OK", "CHECK"]
@@ -19,58 +24,63 @@ def get_obd_code(component):
     status = random.choice(status_options)
     
     if status == "OK":
-        return "N/A"
+        return {"obd_code": "N/A", "translated_message": "No Aplica"}
     elif status in ["CHECK"]:
         for error in odb_codes:
             if component.lower() in error["message"].lower():
-                return error["obd_code"]
-        return "CODE_NOT_FOUND"
+                # Traducción real usando deep-translator
+                translated_message = GoogleTranslator(source='en', target='es').translate(error["message"])
+                return {"obd_code": error["obd_code"], "translated_message": translated_message}
+        return {"obd_code": "CODE_NOT_FOUND", "translated_message": "Código No Encontrado"}
     else:
-        return "N/A"
+        return {"obd_code": "N/A", "translated_message": "No Aplica"}
 
+# OUTPUT EXAMPLE
+# "obd_code":,
+# "translated_message":,
+# "status":
+#
 def generate_status():
-
     data = {
         "km": random.randint(10000, 100000),
         "engine_status": {
-            "obd_code":  get_obd_code("engine"),
-            "status":  random.choice(status_options)
+            **get_obd_code("engine"),
+            "status": random.choice(status_options)
         },
         "battery_status": {
-            "obd_code":  get_obd_code("battery"),
-            "status": "DANGER"
-        }, #Solo este danger
+            **get_obd_code("battery"),
+            "status": "DANGER"  # Siempre DANGER
+        },
         "brakes_status": {
-            "obd_code":  get_obd_code("brakes"),
-            "status":  random.choice(status_options)
+            **get_obd_code("brakes"),
+            "status": random.choice(status_options)
         },
         "tires_status": {
-            "obd_code":  get_obd_code("tires"),
-            "status":  random.choice(status_options)
+            **get_obd_code("tires"),
+            "status": random.choice(status_options)
         },
         "oil_status": {
-            "obd_code":  get_obd_code("oil"),
-            "status":  random.choice(status_options)
+            **get_obd_code("oil"),
+            "status": random.choice(status_options)
         },
         "temperature_status": {
-            "obd_code":  get_obd_code("temperature"),
-            "status":  random.choice(status_options)
+            **get_obd_code("temperature"),
+            "status": random.choice(status_options)
         },
         "front_light_status": {
-            "obd_code":  get_obd_code("front light"),
-            "status":  random.choice(status_options)
+            **get_obd_code("front light"),
+            "status": random.choice(status_options)
         },
         "rear_light_status": {
-            "obd_code":  get_obd_code("rear light"),
-            "status":  random.choice(status_options)
+            **get_obd_code("rear light"),
+            "status": random.choice(status_options)
         },
     }
 
     return data
 
-
-def generate_random_car_status(interval_seconds, vehicle_id):
-
+def odb_sim_status(interval_seconds, vehicle_id):
+     
     url = f"{API_URL}/vehicles/{vehicle_id}/actual-status"
 
     while True:
@@ -78,13 +88,13 @@ def generate_random_car_status(interval_seconds, vehicle_id):
         try:
             response = requests.put(url, json=data)
             if response.status_code == 200:
-                print("Estado: ", data)
+                print("Estado enviado correctamente:", json.dumps(data, ensure_ascii=False, indent=2))
             else:
-                print(f"Error: {response.status_code}, Response: {response.text}")
+                print(f"Error al enviar: {response.status_code}, Response: {response.text}")
         except requests.exceptions.RequestException as e:
             print("Error al enviar:", e)
 
         time.sleep(int(interval_seconds))
 
 
-generate_random_car_status(INTERVAL_SECONDS, VEHICLE_ID)
+odb_sim_status(INTERVAL_SECONDS, VEHICLE_ID)
